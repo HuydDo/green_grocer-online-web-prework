438215710d9f88653ec36561da0b036359ff8cb3
Huy Do <huyddo40@gmail.com>
2019-06-10 07:19:51 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -1,21 +1,25 @@
 def consolidate_cart(cart)
 	hash = {}
   cart.each do |item|
-		# puts "#{item}"
 			item.each do |name,name_hash|
-				puts "#{name}, #{name_hash}"
-				if hash[name]
-					name_hash[:count] += 1
-					puts "#{name_hash}"
-				else
+				if hash[name] #if hash[name] is define
+				  name_hash[:count] ||= 1
+					name_hash[:count] +=1
+					hash[name] = name_hash
+					puts "count: #{name_hash[:count]}"
+					puts "#{name}, #{name_hash}"
+				else 
 					hash[name] ||= {}
+				  puts "#{hash[name]}"
+					hash[name] = name_hash
 					name_hash[:count] =1
-					puts "#{name_hash}"
+					puts "#{hash[name]}"
 				end
 			end
 	end
 	hash
 end
+
 
 def apply_coupons(cart, coupons)
   # code here
